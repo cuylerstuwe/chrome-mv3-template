802dfd7c6de9ad16011adb48c65e91e765c0798e
Cuyler Stuwe <salembeats@gmail.com>
2021-07-19 19:35:00 -0700

    reorganize startedLog.test.js, explain strange import method

--- src/utils/startedLog.test.js
+++ src/utils/startedLog.test.js
@@ -1,6 +1,8 @@
 jest.spyOn(global.console, "log").mockImplementation();
-import("./startedLog");
 
-test("When the startedLog module is imported, it logs 'Started.' to the console as a side-effect.", () => {
+test("When the startedLog module is imported, it logs 'Started.' to the console as a side-effect.", async () => {
+    // ES-style static imports in a test file are hoisted by Jest, unless they're mocked.
+    // startedLog is therefore imported dynamically, as we want to make sure we install the spy on console.log before startedLog runs it as a side-effect of being imported.
+    await import("./startedLog");
     expect(console.log).toBeCalledWith("Started.");
 });
