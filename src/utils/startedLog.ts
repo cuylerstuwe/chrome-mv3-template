b4d2a3bb22f3efa35fc7fb04f8c776a1b2f1b235
Cuyler Stuwe <cuylerkstuwe@gmail.com>
2024-05-23 11:02:05 -0700

    include manifest printout in startedlog

--- src/utils/startedLog.ts
+++ src/utils/startedLog.ts
@@ -1,4 +1,5 @@
 console.groupCollapsed(`Extension context for ${chrome.runtime.getManifest().name} started.`);
+console.log('Manifest:', chrome.runtime.getManifest());
 console.log('process.env.NODE_ENV:', process.env.NODE_ENV);
 console.log('process.env.BUILD_ENV:', process.env.BUILD_ENV);
 console.log('process.env.DEPLOY_MODE:', process.env.DEPLOY_MODE);
