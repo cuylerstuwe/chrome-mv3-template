0be17e9769915e1a0863487f72a75751901cb8be
Cuyler Stuwe <cuylerkstuwe@gmail.com>
2024-05-24 23:05:29 -0700

    cast to return type for better inference

--- src/foreground/utils/sendMessageToBackground.ts
+++ src/foreground/utils/sendMessageToBackground.ts
@@ -1,11 +1,8 @@
 import type { handlers } from "background/serviceWorker/handlers";
 import type { HandlerNames, HandlerParams, MessageFromForeground } from "shared/types/messageFromForeground";
 
-export function sendMessageToBackground<K extends HandlerNames>(
-	type: K,
-	...args: HandlerParams[K]
-): Promise<Awaited<ReturnType<(typeof handlers)[K]>>> {
+export function sendMessageToBackground<K extends HandlerNames>(type: K, ...args: HandlerParams[K]) {
 	return new Promise((resolve) => {
 		chrome.runtime.sendMessage({ type, args } as MessageFromForeground, resolve);
-	});
+	}) as ReturnType<handlers[K]>;
 }
