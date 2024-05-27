7c140b694ac55902fb8c480dffd147eaa6907962
Cuyler Stuwe <cuylerkstuwe@gmail.com>
2024-05-26 20:37:40 -0700

    further explanation for background/listeners/index

--- src/background/listeners/index.ts
+++ src/background/listeners/index.ts
@@ -1,5 +1,8 @@
 /**
  * This is a barrel file that exports every listener.
+ *
+ * Whichever listeners are exported here will be registered by the service worker,
+ * and will be typed and callable from foreground scripts.
  */
 
 export { sumTwoNumbers } from "./implementations/sumTwoNumbers";
