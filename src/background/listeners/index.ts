299e7c2f219afd31015b2e669b2ecf2ef670632c
Cuyler Stuwe <cuylerkstuwe@gmail.com>
2024-05-26 17:36:34 -0700

    delete noop listener (redundant for demo purposes with actual listeners implemented)

--- src/background/listeners/index.ts
+++ src/background/listeners/index.ts
@@ -3,5 +3,4 @@
  */
 
 export { sumTwoNumbers } from "./implementations/sumTwoNumbers";
-export { noop } from "./implementations/noop";
 export { fetchWorldTime } from "./implementations/fetchWorldTime";
