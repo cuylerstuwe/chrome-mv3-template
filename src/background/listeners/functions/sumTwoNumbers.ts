23550173e76b1e70656adbda18499bffa01cd584
Cuyler Stuwe <cuylerkstuwe@gmail.com>
2024-05-26 21:30:12 -0700

    add more pontification to the sumTwoNumbers comment about ipc communication

--- src/background/listeners/functions/sumTwoNumbers.ts
+++ src/background/listeners/functions/sumTwoNumbers.ts
@@ -2,6 +2,8 @@
  * Provided as a demo of how to write a listener that does not perform async operations.
  * Note that this function is marked as async, regardless of whether it actually performs async operations.
  * This is to ensure that the function signature is consistent with other listeners.
+ * The nature of IPC communication means that there's an inherent end-to-end async nature to all listeners anyway.
+ * Embrace it.
  * @param a The first number.
  * @param b The second number.
  */
