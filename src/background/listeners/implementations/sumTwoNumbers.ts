0468c952c9db61aad01da62434358559175b113a
Cuyler Stuwe <cuylerkstuwe@gmail.com>
2024-05-26 17:39:13 -0700

    add note explaining the purpose of sumTwoNumbers

--- src/background/listeners/implementations/sumTwoNumbers.ts
+++ src/background/listeners/implementations/sumTwoNumbers.ts
@@ -1,5 +1,7 @@
 /**
- * Return the sum of two numbers.
+ * Provided as a demo of how to write a listener that does not perform async operations.
+ * Note that this function is marked as async, regardless of whether it actually performs async operations.
+ * This is to ensure that the function signature is consistent with other listeners.
  * @param a The first number.
  * @param b The second number.
  */
