322165f9cb4e0e62d66d96775b1780c6b66eeae7
Cuyler Stuwe <cuylerkstuwe@gmail.com>
2024-06-03 14:36:09 -0700

    reference messageTypes as msg in content script

--- src/foreground/content/content.ts
+++ src/foreground/content/content.ts
@@ -1,13 +1,13 @@
 import "shared/utils/logBootupDiagnostics";
 import {dispatch} from "foreground/utils/dispatch";
 
-import { allMessageTypes as messageTypes } from "foreground/utils/allMessageTypes";
+import { allMessageTypes as msg } from "foreground/utils/allMessageTypes";
 
 async function main() {
-    const myTime = await dispatch(messageTypes.fetchWorldTime);
+    const myTime = await dispatch(msg.fetchWorldTime);
     console.log("The current time for me is", myTime?.asFormattedTime);
 
-    const tokyoTime = await dispatch(messageTypes.fetchWorldTime, "timezone/Asia/Tokyo");
+    const tokyoTime = await dispatch(msg.fetchWorldTime, "timezone/Asia/Tokyo");
     console.log("The current time in Tokyo is:", tokyoTime?.utcDatetime);
 }
 
