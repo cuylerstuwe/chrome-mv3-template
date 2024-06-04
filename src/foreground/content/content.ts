e27d5276e889b3022185caa3ec4931c0e309ebf6
Cuyler Stuwe <cuylerkstuwe@gmail.com>
2024-06-04 08:40:51 -0700

    fix prettier settings in content.ts

--- src/foreground/content/content.ts
+++ src/foreground/content/content.ts
@@ -1,15 +1,14 @@
 import "shared/utils/logBootupDiagnostics";
-import {dispatch} from "foreground/utils/dispatch";
+import { dispatch } from "foreground/utils/dispatch";
 
 import { allMessageTypes as msg } from "foreground/utils/allMessageTypes";
 
 async function main() {
-    const myTime = await dispatch(msg.fetchWorldTime);
-    console.log("The current time for me is", myTime?.asFormattedTime);
+	const myTime = await dispatch(msg.fetchWorldTime);
+	console.log("The current time for me is", myTime?.asFormattedTime);
 
-    const tokyoTime = await dispatch(msg.fetchWorldTime, "timezone/Asia/Tokyo");
-    console.log("The current time in Tokyo is:", tokyoTime?.utcDatetime);
+	const tokyoTime = await dispatch(msg.fetchWorldTime, "timezone/Asia/Tokyo");
+	console.log("The current time in Tokyo is:", tokyoTime?.utcDatetime);
 }
 
-main().then(() => {
-});
+main().then(() => {});
