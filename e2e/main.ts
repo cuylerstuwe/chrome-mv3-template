de37ea9ad163f28b0c85dc2180540661d852fd84
Cuyler Stuwe <cuylerkstuwe@gmail.com>
2024-06-04 13:07:38 -0700

    deepen scaffolding assertions of baseline expectations

--- e2e/main.ts
+++ e2e/main.ts
@@ -9,9 +9,26 @@ import { Browser, Page, WebWorker } from "puppeteer";
  */
 export async function main(browser: Browser, serviceWorker: WebWorker | null, initialTab: Page): Promise<void> {
 	const { expect } = await import("chai");
+
 	expect(browser).not.to.be.undefined;
+	if (browser === null) {
+		throw new Error('The "browser" object is null, when it was expected not to be.');
+	}
+
 	expect(serviceWorker).not.to.be.undefined;
+	if (serviceWorker === null) {
+		throw new Error('The "serviceWorker" object is null, when it was expected not to be.');
+	}
+
 	expect(initialTab).not.to.be.undefined;
+	if (initialTab === null) {
+		throw new Error('The "initialTab" object is null, when it was expected not to be.');
+	}
+
+	expect(serviceWorker.url()).to.be.a("string");
+	expect(serviceWorker.url()).to.have.length.greaterThan(0);
+	expect(serviceWorker.url()).to.include("chrome-extension://");
+	expect(serviceWorker.url()).to.include("serviceWorker.js");
 
 	// Implement tests here.
 }
