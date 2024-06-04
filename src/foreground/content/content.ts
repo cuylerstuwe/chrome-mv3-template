8d584b9d13a6acd8eb8a8ab74de3a1200bcfec71
Cuyler Stuwe <cuylerkstuwe@gmail.com>
2024-06-03 21:21:44 -0700

    experiment: use listeners directly in foreground

--- src/foreground/content/content.ts
+++ src/foreground/content/content.ts
@@ -1,14 +1,12 @@
 import "shared/utils/logBootupDiagnostics";
-import { dispatch } from "foreground/utils/dispatch";
+import {dispatch} from "foreground/utils/dispatch";
 
 import { allMessageTypes as msg } from "foreground/utils/allMessageTypes";
 
 async function main() {
-	const myTime = await dispatch(msg.fetchWorldTime);
-	console.log("The current time for me is", myTime?.asFormattedTime);
-
-	const tokyoTime = await dispatch(msg.fetchWorldTime, "timezone/Asia/Tokyo");
-	console.log("The current time in Tokyo is:", tokyoTime?.utcDatetime);
+    const myTime = await msg.fetchWorldTime();
+    console.log('it worked', myTime);
 }
 
-main().then(() => {});
+main().then(() => {
+});
